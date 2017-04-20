5cc3412ddfd74e2c75566990502640ba01a1c007
Raffy Kaloustian <raffy@piranapool.com>
2017-04-20 16:48:17 -0700

    I think I've got all the issues resolved for an initial release

--- src/Utility.php
+++ src/Utility.php
@@ -8,30 +8,31 @@ class Utility
 {
     public static function postUpdate(Event $event)
     {
-        $collection                     = realpath(__DIR__ . '/Collection.php');
-        $rest_dir                       = realpath(__DIR__ . '/Rest');
-        $rpc_dir                        = realpath(__DIR__ . '/Rpc');
-        $view_dir                       = realpath(__DIR__ . '/../view');
-        $new_apigility_dir              = realpath(__DIR__ . '/../../../../module/Application/src/Apigility');
+        $collection                     = __DIR__ . '/Collection.php';
+        $rest_dir                       = __DIR__ . '/Rest';
+        $rpc_dir                        = __DIR__ . '/Rpc';
+        $view_dir                       = __DIR__ . '/../view';
+
+        $new_apigility_dir              = __DIR__ . '/../../../../module/Application/src/Apigility';
+        $new_apigility_collection       = $new_apigility_dir . '/Collection.php';
+        $new_apigility_rest_dir         = $new_apigility_dir . '/Rest';
+        $new_apigility_rpc_dir         = $new_apigility_dir . '/Rpc';
         $new_apigility_view_dir         = $new_apigility_dir . '/view';
-        $apigility_admin_dir            = realpath(__DIR__ . '/../../../zfcampus/zf-apigility-admin');
+
+        $apigility_admin_dir            = __DIR__ . '/../../../zfcampus/zf-apigility-admin';
         $apigility_admin_view_dir       = $apigility_admin_dir . '/view';
         $apigility_admin_view_out_dir   = $apigility_admin_dir . '/view.out';
 
-        echo $new_apigility_dir . "\n";
-        echo $apigility_admin_dir . "\n";
-
         if (!is_dir($new_apigility_dir)) {
             `mkdir $new_apigility_dir`;
-/*
-            `cp $collection $new_apigility_dir`;
-            `cp -r $rest_dir $new_apigility_dir`;
-            `cp -r $rpc_dir $new_apigility_dir`;
-            `cp -r $view $new_apigility_dir`;
+
+            `cp $collection $new_apigility_collection`;
+            `cp -r $rest_dir $new_apigility_rest_dir`;
+            `cp -r $rpc_dir $new_apigility_rpc_dir`;
+            `cp -r $view $new_apigility_view_dir`;
 
             `mv $apigility_admin_view_dir $apigility_admin_view_out_dir`;
             `ln -s $new_apigility_view_dir $apigility_admin_view_dir`;
-*/
         }
     }
 }
